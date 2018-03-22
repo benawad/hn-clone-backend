877fdb56c7720fcf8770bacf19ec8a42462c4995
ben awad <benawad97@gmail.com>
2018-03-22 12:42:33 -0500

    add mutations for login and signup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { APP_SECRET } = require('./utils');
 
 const resolvers = {
   Query: {
@@ -14,6 +18,40 @@ const resolvers = {
   Mutation: {
     post(parent, { url, description }, ctx, info) {
       return ctx.db.mutation.createLink({ data: { url, description } }, info);
+    },
+    async signup(parent, args, ctx, info) {
+      const password = await bcrypt.hash(args.password, 10);
+      const user = await ctx.db.mutation.createUser(
+        {
+          data: { ...args, password },
+        },
+        info,
+      );
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+      return {
+        token,
+        user,
+      };
+    },
+    async login(parent, args, ctx, info) {
+      const user = await ctx.db.query.user({ where: { email: args.email } }, info);
+      if (!user) {
+        throw new Error(`Could not find user with email: ${args.email}`);
+      }
+
+      const valid = await bcrypt.compare(args.password, user.password);
+      if (!valid) {
+        throw new Error('Invalid password');
+      }
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+      return {
+        token,
+        user,
+      };
     },
   },
 };
---
+++ src/utils.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line
+export const APP_SECRET = 'GraphQL-is-aw3some';
